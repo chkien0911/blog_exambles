539e46d7fc67ce87f52cb2486dbc786e5af7a065
Kien Chu <kien.chu@tctav.com>
2017-07-23 15:55:36 +0700

    1st

---
+++ AngularJs4/AngularJs4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJs4.Startup))]
+namespace AngularJs4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
